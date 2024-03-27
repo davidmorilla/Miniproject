e9ce1cb15f7bd888289c4980a47737ad7e83ebf2
MORILLA SORLÍ, DAVID <david.morilla@alumnos.upm.es>
2024-03-27 16:32:47 +0100

    Add custom exception

---
+++ src/main/java/es/upm/pproject/miniproject/miniproject/BlankInputException.java
@@ -0,0 +1,9 @@
+package es.upm.pproject.miniproject.miniproject;
+
+public class BlankInputException extends Exception {
+    
+   public BlankInputException(String errorMessage){
+        super(errorMessage);
+    }
+    
+}
