8ef1e7e5f9a29765db57510207bb0ff95aae72f3
David Morilla <david.morilla@alumnos.upm.es>
2024-04-01 20:46:43 +0200

    Adding more information in logs

--- src/main/java/es/upm/pproject/miniproject/miniproject/EnrollmentManager.java
+++ src/main/java/es/upm/pproject/miniproject/miniproject/EnrollmentManager.java
@@ -23,46 +23,49 @@ public class EnrollmentManager implements InterfaceEnrollmentManager{
 	}
 	
 	public void registerCourse(int code, String name, String coordinator) throws CourseAlreadyExistsException, CourseBlankInputException {
+		logger.info(String.format("--Resgistering course...\n\tCode: %d\n\tName: %s\n\tCoordinator: %s", code, name, coordinator));
 		if(courses.get(code)==null) {
 			courses.put(code, new Course(code, name, coordinator));
 			enrollment.put(code, new ArrayList<Student>());	
-			logger.info("Course registered in the system");
+			logger.info(String.format("--Course registered in the system\n\tCode: %d\n\tName: %s\n\tCoordinator: %s", code, name, coordinator));
 		}else {
-			logger.error("Course already registered in the system");
+			logger.error(String.format("Course with code: %d already registered in the system", code));
 			throw new CourseAlreadyExistsException();
 		}
 	}
 	
 	public void registerStudent(int id, String name, String email) throws StudentAlreadyExistsException, StudentBlankInputException, EmailFormatException {
+		logger.info(String.format("--Registering student...\n\tIdentification: %d\n\tName: %s\n\tE-mail: %s", id, name, email));
 		if(students.get(id)==null) {
 			students.put(id, new Student(id, name, email));
-			logger.info("Student registered in the system");
+			logger.info(String.format("--Student registered in the system\n\tIdentification: %d\n\tName: %s\n\tE-mail: %s", id, name, email));
 		}else {
-			logger.error("Student already registered in the system");
+			logger.error(String.format("--Student with identification: %s already registered in the system", id));
 			throw new StudentAlreadyExistsException();
 		}
 	}
 	
 	public void enroll(int course_code, int student_id) throws StudentAlreadyEnrolledException, FullCourseException, MissingStudentException, MissingCourseException {
+		logger.info(String.format("--Enrolling student...\n\tIdentification: %d\n in course\n\tCode: %d", student_id, course_code));
 		if(students.get(student_id)!= null && courses.get(course_code)!=null) {
 			List<Student> students_enrolled = enrollment.get(course_code);
 			if(students_enrolled.size()<50) {
 				if(!isEnrolled(student_id, students_enrolled)) {
 					sortedInsert(course_code, student_id, students_enrolled);
-					logger.info("Student enrolled in the course");
+					logger.info(String.format("--Student \n\tIdentification: %d\n enrolled in course\n\tCode: %d", student_id, course_code));
 				} else {
-					logger.error("Student already enrolled in the course");
+					logger.error("--Student already enrolled in the course" );
 					throw new StudentAlreadyEnrolledException();
 				}
 			} else {
-				logger.error("There are already 50 students enrolled in " + courses.get(course_code).getName() + ".");
+				logger.error("--There are already 50 students enrolled in " + course_code +".");
 				throw new FullCourseException();
 			}
 		} else if(students.get(student_id)== null) {
-			logger.error("Student was not registered in the system");
+			logger.error("--Student" + student_id + " was not registered in the system");
 			throw new MissingStudentException();
 		} else {
-			logger.error("Course was not registered in the system");
+			logger.error("--Course"+course_code+" was not registered in the system");
 			throw new MissingCourseException();
 		}
 	}
@@ -96,10 +99,17 @@ public class EnrollmentManager implements InterfaceEnrollmentManager{
 
     public List<Student> getStudentsEnrolledInCourse(int course) throws MissingCourseException {
     	if(courses.get(course)==null) {
-    		logger.error("Course was not registered in the system");
+    		logger.error("--Course"+course+" was not registered in the system");
     		throw new MissingCourseException();
     	}
-    	logger.info("Students enrolled in the course have been obtained");
+    	String a= "--Students enrolled in the course have been obtained:\n";
+    	
+    	for(int i=0; i<enrollment.get(course).size(); i++) {
+    		Student s= new ArrayList<Student>(enrollment.get(course)).get(i);
+    		
+    		a += String.format("\tIdentification: %d\n\tName: %s\n\tE-mail: %s\n\n", s.getId(),s.getName(),s.getEmail_address()) ;
+    	}
+    	logger.info(a);
        	return enrollment.get(course);
     }
     
